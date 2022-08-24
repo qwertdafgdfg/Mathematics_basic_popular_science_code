679e10571609ed3e5f4f646ffcfc1284971dc276
Admin <619418777@qq.com>
2022-08-24 17:08:05 +0800

    aaaa

---
+++ src/main/java/com/idea/math/test/my04/SegmentationCollectionUtils.java
@@ -0,0 +1,12 @@
+package com.idea.math.test.my04;
+
+/**
+ * @author: liYuan
+ * @Title: SegmentationCollectionUtils
+ * @ProjectName: Mathematics_basic_popular_science_code
+ * @Description:
+ * @date: 2022/8/9 7:35
+ */
+public class SegmentationCollectionUtils {
+
+}
